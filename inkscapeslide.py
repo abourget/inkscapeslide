c682e16f207acb197d837cb492d968e9ae27733a
Alexandre Bourget <alex@bourget.cc>
2009-03-19 08:38:10 -0400

    Security checks and modifications by Felipe. Thanks for you contribution.
    Signed-off-by: Felipe <felipe.andres.manzano@gmail.com>

--- inkscapeslide.py
+++ inkscapeslide.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -=- encoding: utf-8 -=-
-# Author: Alexandre Bourget
-# Copyright (c) 2008: Alexandre Bourget
+# Author: Alexandre Bourget 
+# Copyright (c) 2008: Alexandre Bourget 
 # LICENSE: GPLv3
 
 
 import lxml.etree
 import sys
 import os
-
+import re
 
 
 # Grab user arguments
@@ -57,7 +57,7 @@ if not content_layer:
 
 content = content_layer[0]
 
-# Find the text stuff, everythign starting with SLIDE:
+# Find the text stuff, everything starting with SLIDE:
 #   take all the layer names separated by ','..
 preslides = [x.text for x in content.findall('{http://www.w3.org/2000/svg}text/{http://www.w3.org/2000/svg}tspan') if x.text]
 
@@ -76,31 +76,41 @@ for sl in preslides:
     if sl:
         slides.append([x.strip() for x in sl.split(',')])
 
+pdfslides = []
 for i, slide in enumerate(slides):
     # Hide all layers
     for l in layers:
-        l.attrib['style'] = 'display:none'
+    	# Set display mode to none
+        l.attrib['style'] = re.sub(r'(.*display:)([a-zA-Z]*)(.*)', r'\1none\3', 
+                                   l.attrib['style'])
 
     # Show only slide layers
     for l in layers:
         if l.attrib.get('{http://www.inkscape.org/namespaces/inkscape}label') in slide:
-            l.attrib['style'] = 'display:inline'
+	    # Set display mode to inline
+    	    l.attrib['style'] = re.sub(r'(.*display:)([a-zA-Z]*)(.*)',
+                                       r'\1inline\3', l.attrib['style'])
     
+    svgslide = "%s.p%d.svg" % (FILENAME, i)
+    pdfslide = "%s.p%d.pdf" % (FILENAME, i)
     # Write the XML to file, "wireframes.p1.svg"
-    f = open(FILENAME+".p%d.svg" % i, 'w')
+    f = open(svgslide, 'w')
     f.write(lxml.etree.tostring(doc))
     f.close()
     
     # Run inkscape -A wireframes.p1.pdf wireframes.p1.svg
-    os.system("inkscape -A %s.p%d.pdf %s.p%d.svg" % (FILENAME, i, FILENAME, i))
+    os.system("inkscape -A %s %s" % (pdfslide, svgslide))
+    os.unlink(svgslide)
+    pdfslides.append(pdfslide)
+
     print "Generated page %d." % (i+1)
 
-
 # In the end, run: pdfjoin wireframes.p*.pdf -o Wireframes.pdf
-os.system("pdfjoin --outfile %s.pdf %s.p*.pdf" % (FILENAME, FILENAME))
+os.system("pdfjoin --outfile %s.pdf %s" % (FILENAME, " ".join(pdfslides)))
 
 # Clean up
-os.system("rm -f %s.p*.pdf %s.p*.svg" % (FILENAME, FILENAME))
+for pdfslide in pdfslides:
+    os.unlink(pdfslide)
 
 
 
